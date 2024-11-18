92b45ebe211dd0d663a4dd98f4d9fb19b5e6cbd7
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-18 22:14:41 +0200

    node day1

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const fc = fs.readFileSync("./test.text", "utf8");
 console.log(fc);
 console.log(__filename);
 console.log(__dirname);
+const s = require("./module");
+// delete require.cache["filename"];
+s.sum(1, 2);
