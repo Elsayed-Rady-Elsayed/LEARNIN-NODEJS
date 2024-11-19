b9fca51bbe7e62c64a95a324c0b7b3b34618b75c
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-19 20:45:12 +0200

    cli with nodejs

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-const { pbkdf2 } = require("crypto");
-const start = performance.now();
-pbkdf2("secret", "salt", 100000, 64, "sha512", () => {});
-console.log("end of function", performance.now() - start);
+// const { pbkdf2 } = require("crypto");
+// process.env.UV_THREADPOOL_SIZE = 4;
+// const start = performance.now();
+// pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("end of function", performance.now() - start);
+// });
+// pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("end of function", performance.now() - start);
+// });
+// pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("end of function", performance.now() - start);
+// });
+// pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("end of function", performance.now() - start);
+// });
+// pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("end of function", performance.now() - start);
+// });
+// pbkdf2("secret", "salt", 100000, 64, "sha512", () => {
+//   console.log("end of function", performance.now() - start);
+// });
+const http = require("node:http");
+const server = http.createServer((req, res) => {
+  res.end("hello world");
+});
+server.listen(3001, () => {
+  console.log("listheinig on port 3001");
+});
